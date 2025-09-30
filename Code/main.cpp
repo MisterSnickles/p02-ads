22cfec07634633337a433bda49b8cd9f3fa9f264
MisterSnickles <papasnickles31@gmail.com>
2025-09-29 21:47:08 -0500

    JM: implemented todo 1

--- Code/main.cpp
+++ Code/main.cpp
@@ -12,11 +12,71 @@ using namespace std;
 Grid<char>* read_input(string file, string& target) {
 
     // TODO 1: read the input file
+    ifstream inputFile(file);
+    if (!inputFile) {
+        throw FileException(file);
+    }
+
+    // get first line from input file and assign it to target
+    getline(inputFile, target);
+    if (!isUpperLetter(target)) {
+        throw FileContentException();
+    }
+
+    // initialize variables for rows and columns
+    string rows;
+    string columns;
+    // get second line  from input file and assign first digit to row and second digit to column
+    inputFile >> rows >> columns;
+
+    if (!isNumber(rows) || !isNumber(columns)) {
+        throw FileContentException();
+    }
+
+    int num_rows = string_to_int(rows);
+    int num_columns = string_to_int(columns);
+
+    if (num_rows <= 0 || num_columns <= 0) {
+        throw FileContentException();
+    }
+
+
+    // read follwing lines into a grid (2d array)
     
+    char** grid_data = new char*[num_rows];
+    for (int i = 0; i < num_rows; i++) {
+        grid_data[i] = new char[num_columns];
+        string line;
+
+        // throw error if line to short or read fails
+        if (!getline(inputFile, line) || line.length() != num_columns) {
+
+            // properly delete grid data before throwing error
+            for (int j = 0; j < i; j++) {
+                delete[] grid_data[j];
+            } 
+            // delete object and throw exception
+            delete grid_data;
+            throw FileContentException();
+        }
+
+        // if no error
+        for (int j = 0; j < num_columns; j++) {
+            grid_data[i][j] = line[j];
+        }
+    }
+
+    // create new grid on heap using row and column as parameters
+    Grid<char>* grid = new Grid<char>(num_rows, num_columns, grid_data);
+
+    // clean up temp grid_data
+    for (int i = 0; i < num_rows; i++) {
+        delete[] grid_data[i];
+    } 
+    delete[] grid_data;
 
 
-
-    return nullptr;
+    return grid;
 }
 
 /*
