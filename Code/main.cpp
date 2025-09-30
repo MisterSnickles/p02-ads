058c7f4495be97a94b0816902133129995890a29
Andrew Aprile <andrew.aprile1@gmail.com>
2025-09-30 15:57:23 -0500

    working on bonus 1

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,5 +1,5 @@
 #include <fstream>
-//#include <vector>
+#include <vector>
 #include "Utility.h"
 #include "Grid.h"
 
@@ -87,13 +87,13 @@ bool search(Grid<char>* grid, int r, int c, const char* target) {
     }
     
     // make a temporary variable to store the character in the premade grid
-    char getcharacter;
+    char getGridCharacter;
     // gets the variable from the grid
-    grid->get(r,c,getcharacter);
+    grid->get(r,c,getGridCharacter);
     // see if it equals the current value of the character in target. 
     //      remember, *target is an array of characters. 
     //      you are comparing a character in the grid to the current character in your word
-    if (getcharacter != *target){
+    if (getGridCharacter != *target){
         return false;
     }
     
@@ -103,25 +103,77 @@ bool search(Grid<char>* grid, int r, int c, const char* target) {
     grid->set(r, c, '\0');
     
     //  Recursive Search the grid. down, up, left, right
-    //              | row,col |
-    //              | 1-1,1+0  |
-    //    | 1+0,1-1 |   1,1    | 1+0,1+1 |
-    //              | 1+1,1+0  |     
-    result =  search(grid, r-1, c, target+1)     // search down.    1 row up, same column
-            ||search(grid, r+1, c, target+1)     // search up.      1 row down, same column
-            ||search(grid, r, c-1, target+1)     // search left.    1 column to the left, same row
-            ||search(grid, r, c+1, target+1);    // search right.   1 column to the right, same row
+    //              | row, col|
+    //              | r-1, c  |
+    //    | r, c-1  |   r, c  | r, c+1 |
+    //              | r+1, c  |     
+    result =  search(grid, r-1, c, target+1)     // search up.      1 row up,               same column
+            ||search(grid, r+1, c, target+1)     // search down.    1 row down,             same column
+            ||search(grid, r, c-1, target+1)     // search left.    1 column to the left,   same row
+            ||search(grid, r, c+1, target+1);    // search right.   1 column to the right,  same row
 
 
-    //reset the grid box back to the original character as you backtrack
-    grid->set(r, c, getcharacter);
+    // reset the grid box back to the original character as you backtrack
+    grid->set(r, c, getGridCharacter);
 
     return result;
 }
 
 // TODO bonus: search the target in the grid, and print the sequence(s) into the output stream
 // e.g., you can consider the following function:
-// void search2(Grid<char>* grid, int r, int c, const char* target, vector<string>& result, ostream& out);
+void search2(Grid<char>* grid, int r, int c, const char* target, vector<string>& result, ostream& out){
+//   bool true/false = false;
+    // // TODO 2: search the target in the grid
+
+    //Base Case. You got to the end of the character string.
+    if (*target == '\0'){
+        return;
+    }
+
+    //check that you aren't looking in a grid box out side of the range.
+    if (r<0 || c<0)
+        return;
+    // checks to see if the number of rows and columns are less than the what the function called for
+    if (grid->row() <= r || grid->col() <= c){
+        return;
+    }
+    
+    // make a temporary variable to store the character in the premade grid
+    char getGridCharacter;
+    // gets the variable from the grid
+    grid->get(r,c,getGridCharacter);
+    // see if it equals the current value of the character in target. 
+    //      remember, *target is an array of characters. 
+    //      you are comparing a character in the grid to the current character in your word
+    if (getGridCharacter != *target){
+        return;
+    }
+    
+    out << "(" << r << "," << c << ") " ;
+
+    // set the current grid box to \0. 
+    // this makes it so that if you get to it again during search,
+    //      when you are doing your 4 way search, it gets ignored
+    grid->set(r, c, '\0');
+    
+    //  Recursive Search the grid. down, up, left, right
+    //              | row, col|
+    //              | r-1, c  |
+    //    | r, c-1  |   r, c  | r, c+1 |
+    //              | r+1, c  |     
+    search2(grid, r-1, c, target+1, result, out);     // search up.      1 row up,               same column
+    search2(grid, r+1, c, target+1, result, out);     // search down.    1 row down,             same column
+    search2(grid, r, c-1, target+1, result, out);     // search left.    1 column to the left,   same row
+    search2(grid, r, c+1, target+1, result, out);    // search right.   1 column to the right,  same row
+
+
+    // reset the grid box back to the original character as you backtrack
+    grid->set(r, c, getGridCharacter);
+
+    return;
+}
+
+
 
 int main(int argc, char* argv[]) {
     try {
@@ -156,15 +208,25 @@ int main(int argc, char* argv[]) {
             throw FileException(argv[2]);
         }
         search(grid, 0 , 1, target.c_str());
+        vector<string>& result = *(new vector<string>);
 
         for (int i = 0; i < grid->row(); ++i) {
             for (int j = 0; j < grid->col(); ++j) {
                 if (search(grid, i , j, target.c_str())) {
-                   output << "Solution found!" << endl;               
-                   cout << "Solution found!" << endl;
+                    cout << "Solution found!" << endl;
+                    output << "Solution found!" << endl;               
+                    search2(grid, i , j, target.c_str(), result, output);
                 }
             }
         }
+        
+        
+
+        for (int i = 0; i < grid->row(); ++i) {
+            for (int j = 0; j < grid->col(); ++j) {
+            }
+        }
+            
         
         output.close();
 
