04d9b5a8f2df1d0d4602488afdb72673d9c25a48
Andrew Aprile <andrew.aprile1@gmail.com>
2025-10-01 22:32:26 -0500

    Cleaned up some stuff

--- Code/main.cpp
+++ Code/main.cpp
@@ -5,61 +5,60 @@
 
 using namespace std;
 
+void print_grid(Grid<char>* grid) {
+    cout << "-----Print Grid------" << endl;
+    for (int i=0; i<(grid->row()); i++){
+        for (int j=0; j<(grid->col()); j++){
+            char getCharacter;
+            grid->get(i,j,getCharacter);
+            cout << getCharacter << " ";
+        }
+        cout << endl;
+    }
+    cout << "-----------------" << endl;
+}
+
 /* 
     The search target string will be saved in string& target
     The grid of letters will be returned
 */
 Grid<char>* read_input(string file, string& target) {
-// TODO 1: read the input file
+    // TODO 1: read the input file
     
     //create stream, open file, and check if it fails.
-    ifstream input;
-    input.open(file);
-    if (input.fail()){
-        throw FileException(file);
-    }
+    ifstream inputFile(file);
+    if (inputFile.fail())   throw FileException(file);
 
     //gets the target word from the first line in the input file
-    getline(input, target);
+    getline(inputFile, target);
     
     //gets the row and column number from the second line in the input file
-    int row;
-    int col;
-    input >> row;
-    input >> col;
+    int numRows;
+    int numCols;
+    inputFile >> numRows;
+    inputFile >> numCols;
+
+    if(numRows <= 0 || numCols <= 0) throw FileContentException();
 
     //Create a new grid object and initialize with the row and column you got from the file
-    Grid<char>* grid = new Grid<char>(row, col);
+    Grid<char>* grid = new Grid<char>(numRows, numCols);
   
     // set each element in the array with the character in the file
-    for (int i=0; i<row; i++){
-        for (int j=0; j<col; j++){
-            char x;
-            if (!(input>>x)){
+    for (int i=0; i<numRows; i++){
+        for (int j=0; j<numCols; j++){
+            char ch;
+            if (!(inputFile>>ch)){
                 throw FileContentException();
             }
-            grid->set(i,j,x);
+            grid->set(i, j, ch);
         }
     }
 
     //close the file
-    input.close();
+    inputFile.close();
 
-    // // Uncomment the code below to print out the data from the file. 
-    // // It prints out the target word, row and columns, and matrix data
-    // cout << "-----After Read Input------" << endl;
-    // cout << "-----You are in the read_input function" << endl;
-    // cout << target << endl;
-    // cout << grid->row() << " " << grid->col() << endl;
-    // for (int i=0; i<(grid->row()); i++){
-    //     for (int j=0; j<(grid->col()); j++){
-    //         char getCharacter;
-    //         grid->get(i,j,getCharacter);
-    //         cout << getCharacter << " ";
-    //     }
-    //     cout << endl;
-    // }
-    // cout << "-----------------" << endl;
+    // // Uncomment me to print the grid to the console for debugging
+    //print_grid(grid);
 
     //return the grid you made. Our grid in main will be set to this grid.
     return grid;
@@ -74,17 +73,13 @@ bool search(Grid<char>* grid, int r, int c, const char* target) {
     // // TODO 2: search the target in the grid
     
     //Base Case. You got to the end of the character string.
-    if (*target == '\0'){
-        return true;
-    }
+    if (*target == '\0') return true;
 
     //check that you aren't looking in a grid box out side of the range.
-    if (r<0 || c<0)
-        return false;
+    if (r<0 || c<0) return false;
     // checks to see if the number of rows and columns are less than the what the function called for
-    if (grid->row() <= r || grid->col() <= c){
-        return false;
-    }
+    if (grid->row() <= r || grid->col() <= c)   return false;
+
     
     // make a temporary variable to store the character in the premade grid
     char getGridCharacter;
@@ -93,9 +88,7 @@ bool search(Grid<char>* grid, int r, int c, const char* target) {
     // see if it equals the current value of the character in target. 
     //      remember, *target is an array of characters. 
     //      you are comparing a character in the grid to the current character in your word
-    if (getGridCharacter != *target){
-        return false;
-    }
+    if (getGridCharacter != *target) return false;
     
     // set the current grid box to \0. 
     // this makes it so that if you get to it again during search,
@@ -186,21 +179,8 @@ int main(int argc, char* argv[]) {
             return EXIT_FAILURE;
         }
 
-        // Uncomment the code below to print out the data from the file. 
-        // It prints out the target word, row and columns, and matrix data
-        cout << "-----Grid Data------" << endl;
-        cout << "-----In main()-----" << endl;
-        cout << target << endl;
-        cout << grid->row() << " " << grid->col() << endl;
-        for (int i=0; i<(grid->row()); i++){
-            for (int j=0; j<(grid->col()); j++){
-                char getCharacter;
-                grid->get(i,j,getCharacter);
-                cout << getCharacter << " ";
-            }
-            cout << endl;
-        }
-        cout << "-----------------" << endl;
+        // // Uncomment me to print the grid to the console for debugging
+        //print_grid(grid);
 
         ofstream output;
         output.open(argv[2]);
